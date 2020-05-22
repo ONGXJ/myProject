c93d46d3ec78496331ef5dc2f66fd623b2324d80
ONGXJ <xiongjie0305@gmail.com>
2020-05-22 15:15:22 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cleaningservice";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+	die("connection failed: " . $conn->connect_error);
+}
+
+?>
